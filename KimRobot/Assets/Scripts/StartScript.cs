ee83cb4c6b5ef4bed442e6bba78696d743e40870
Vicky <69034338+sjin11125@users.noreply.github.com>
2022-08-18 13:34:18 +0900

    .

--- KimRobot/Assets/Scripts/StartScript.cs
+++ KimRobot/Assets/Scripts/StartScript.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
     public GameObject PlayerObj;
     public PlayerController Player;
     public GameObject StartPlayer;
+
+
+    public AudioSource GlassBroken;          //실린더 깨지는 사운드
+    public AudioSource CylinderWarning;          //실린더 경고 사운드
+
+    public bool isStart = false;
+    public bool isStartDone = false;
+    Animator StartAnimation;            //시작 애니메이션
+    Transform[] Trs;
     // Start is called before the first frame update
     void Start()
     {
-       
-    }
 
+        //Tr = gameObject.GetComponent<Transform>();
+        Trs = gameObject.GetComponentsInChildren<Transform>();
+        isStart = false;
+        StartAnimation = GetComponent<Animator>();
+    }
+    IEnumerator StartAnimationCo()
+    {
+        Trs[3].gameObject.SetActive(false);
+        StartAnimation.SetTrigger("WakeUp");
+        yield return new WaitForSeconds(6f);
+        //transform.position = StartPos.transform.position;
+
+        isStartDone = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (isStart)
+        {
+            isStart = false;
+            StartCoroutine(StartAnimationCo());
+
+        }
+        if (isStartDone)
+        {
+            StopCoroutine(StartAnimationCo());
+            //Trs[3].gameObject.SetActive(true);
+            if (GetComponent<StartScript>() != null)
+            {
+
+                GetComponent<StartScript>().StartPos();
+            }
+            SceneManager.LoadScene("MainGame");
+        }
     }
     public void StartPos()
     {
